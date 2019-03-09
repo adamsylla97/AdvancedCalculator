1b40fe789b56f2ed77b272931daa9c4b7d3bec6c
adamsylla97 <209474@edu.p.lodz.pl>
2019-03-09 15:22:31 +0100

    Added calc result layout Added calc buttons layout Added styles for all buttons Added colors for all styles Added activity_simple_calc where we will combine both result and buttons layouts

---
+++ app/src/main/java/com/example/advancedcalculator/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package com.example.advancedcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_calc_buttons);
+    }
+}
