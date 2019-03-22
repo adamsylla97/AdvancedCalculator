ba242b7abfffb59bbf810898402445b7a36e18a1
adamsylla97 <209474@edu.p.lodz.pl>
2019-03-22 22:43:17 +0100

    *redo operation memory and result memory sending data *added chaging sign at the end of operation memory if last char is sign *added clearing result memory after clicking sign button

--- app/src/main/java/com/example/advancedcalculator/SimpleCalculator.java
+++ app/src/main/java/com/example/advancedcalculator/SimpleCalculator.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimpleCalculator extends AppCompatActivity {
 
     TextView operationMemoryTextView;
@@ -14,76 +17,103 @@ public class SimpleCalculator extends AppCompatActivity {
     StringBuilder resultMemory = new StringBuilder();
     StringBuilder operationMemory = new StringBuilder();
 
-    private boolean isSign(String sign){
-        if(sign.equals("+") || sign.equals("-") || sign.equals("*") || sign.equals("/")){
+    private boolean isSign(String sign) {
+        if (sign.equals("+") || sign.equals("-") || sign.equals("*") || sign.equals("/")) {
             return true;
         }
         return false;
     }
 
-    private boolean isNumber(String number){
-        if(number.matches("\\d")){
+    private boolean isNumber(String number) {
+        if (number.matches("\\d")) {
             return true;
         }
         return false;
     }
 
-    public void deleteFromMemory(View view){
+    public void deleteFromMemory(View view) {
 //        if(operationMemory.length()!=0){
 //            operationMemory = operationMemory.substring(0,operationMemory.length()-1);
 //            updateMemoryTextView();
 //        }
     }
 
-    private void getResult(StringBuilder operationMemory){
+    private void getResult(StringBuilder operationMemory) {
         String tempMemory = operationMemory.toString();
         StringBuilder tempNumber = new StringBuilder();
-        Double numberOne = 0.0;
-        Double numberTwo = 0.0;
-        for(int i=0; i<tempMemory.length(); i++){
-            if(isNumber(String.valueOf(tempMemory.charAt(i)))){
-                tempNumber.append(tempMemory.charAt(i));
-            } else if(isSign(String.valueOf(tempMemory.charAt(i)))){
+        String[] tempStringTab = operationMemory.toString().split(" ");
+        List<Double> numbersList = new ArrayList<>();
+        List<String> signsList = new ArrayList<>();
 
+        for (int i = 0; i < tempStringTab.length; i++) {
+
+            if (isNumber(tempStringTab[i])) {
+                numbersList.add(Double.parseDouble(tempStringTab[i]));
             }
+
+            if (isSign(tempStringTab[i])) {
+                if (tempStringTab[i].equals("+") || tempStringTab[i].equals("-")) {
+                    signsList.add(tempStringTab[i]);
+                }
+            }
+
         }
+
     }
 
-    public void pressButton(View view){
+    Integer rej1 = null;
+    Integer rej2 = null;
+    String operator = "";
+
+    public void pressButton(View view) {
         Button pressedButton = (Button) view;
         Log.i("Button pressed:", pressedButton.getTag().toString());
         String pressedButtonTag = "";
         pressedButtonTag = pressedButton.getTag().toString();
 
         if(isNumber(pressedButtonTag)){
+
             resultMemory.append(pressedButtonTag);
+
         }
 
         if(isSign(pressedButtonTag)){
-            operationMemory.append(resultMemory).append(pressedButtonTag);
-            resultMemory.delete(0,resultMemory.length());
+
+            if(resultMemory.length()!=0){
+                operationMemory.append(resultMemory).append(" ");
+                resultMemory.delete(0,resultMemory.length());
+            }
+
+            if(!isSign(String.valueOf(operationMemory.charAt(operationMemory.length()-2)))){
+                operationMemory.append(pressedButtonTag).append(" ");
+            }
+
+            if(operationMemory.length()!=0 && isSign(String.valueOf(operationMemory.charAt(operationMemory.length()-2)))){
+                operationMemory.delete(operationMemory.length()-2,operationMemory.length());
+                operationMemory.append(pressedButtonTag).append(" ");
+            }
+
         }
 
-        Log.i("OPERATIONAL MEMORY:",operationMemory.toString());
 
         updateMemoryTextView();
         updateResultTextView();
 
     }
 
-    private void updateMemoryTextView(){
-        if(operationMemory.length()<22){
+    private void updateMemoryTextView() {
+        if (operationMemory.length() < 22) {
             operationMemoryTextView.setText(operationMemory);
         } else {
-            operationMemoryTextView.setText(operationMemory.substring(operationMemory.length()-22));
+            operationMemoryTextView.setText(operationMemory.substring(operationMemory.length() - 22));
         }
     }
 
-    private void updateResultTextView(){
-        if(resultMemory.length()<11){
+    private void updateResultTextView() {
+        if (resultMemory.length() < 11) {
             resultTextView.setText(resultMemory);
         } else {
-            resultTextView.setText(resultMemory.substring(resultMemory.length()-11));
+            resultTextView.setText(resultMemory.substring(resultMemory.length() - 11));
         }
     }
 
