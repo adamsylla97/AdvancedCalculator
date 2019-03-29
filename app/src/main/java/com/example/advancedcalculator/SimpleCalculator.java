e747d01d33fe78bb28d887f6ee38b0cc634b890a
adamsylla97 <209474@edu.p.lodz.pl>
2019-03-29 23:21:23 +0100

    *implemented "." button functionality *fixed double "." and "0" appearance

--- app/src/main/java/com/example/advancedcalculator/SimpleCalculator.java
+++ app/src/main/java/com/example/advancedcalculator/SimpleCalculator.java
@@ -35,164 +35,156 @@ public class SimpleCalculator extends AppCompatActivity {
         return false;
     }
 
-    public void deleteFromMemory(View view) {
-//        if(operationMemory.length()!=0){
-//            operationMemory = operationMemory.substring(0,operationMemory.length()-1);
-//            updateMemoryTextView();
-//        }
-    }
-
-
     String rej1 = "";
     String rej2 = "";
     String operator = "";
 
-    public void setOperator(String sign){
+    public void setOperator(String sign) {
         operator = sign;
     }
 
-    public void setRegisters(){
+    public void setRegisters() {
 
-        if(rej1.length() == 0){
+        if (rej1.length() == 0) {
             rej1 = resultMemory.toString();
         } else {
             rej2 = resultMemory.toString();
         }
 
-        Log.i("rej1",rej1);
-        Log.i("rej2",rej2);
+        Log.i("rej1", rej1);
+        Log.i("rej2", rej2);
     }
 
     private Double mainResult = 0.0;
 
-    public Double addition(){
-        try{
+    public Double addition() {
+        try {
 
             Double result = 0.0;
             result = Double.parseDouble(rej1) + Double.parseDouble(rej2);
             rej1 = result.toString();
             return result;
 
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
 
     }
 
-    public Double subtraction(){
-        try{
+    public Double subtraction() {
+        try {
 
             Double result = 0.0;
             result = Double.parseDouble(rej1) - Double.parseDouble(rej2);
             rej1 = result.toString();
             return result;
 
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
 
     }
 
-    public Double multiplication(){
-        try{
+    public Double multiplication() {
+        try {
 
             Double result = 0.0;
             result = Double.parseDouble(rej1) * Double.parseDouble(rej2);
             rej1 = result.toString();
             return result;
 
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
 
     }
 
-    public Double division(){
-        try{
+    public Double division() {
+        try {
 
             Double result = 0.0;
             result = Double.parseDouble(rej1) / Double.parseDouble(rej2);
 
 
-            if(Double.parseDouble(rej2) == 0.0){
+            if (Double.parseDouble(rej2) == 0.0) {
                 result = 0.0;
-                Toast.makeText(SimpleCalculator.this,"Nie dziel przez 0!",Toast.LENGTH_LONG).show();
+                Toast.makeText(SimpleCalculator.this, "Nie dziel przez 0!", Toast.LENGTH_LONG).show();
             }
 
             rej1 = result.toString();
 
             return result;
 
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
 
     }
 
-    public void computeResult(){
+    public void computeResult() {
 
-        if(rej1.length() != 0 && rej2.length() != 0){
+        if (rej1.length() != 0 && rej2.length() != 0) {
 
-            switch (operator){
+            switch (operator) {
                 case "+":
                     mainResult = addition();
-                    Log.i("main result",mainResult.toString());
+                    Log.i("main result", mainResult.toString());
                     break;
                 case "-":
                     mainResult = subtraction();
-                    Log.i("main result",mainResult.toString());
+                    Log.i("main result", mainResult.toString());
                     break;
                 case "*":
                     mainResult = multiplication();
-                    Log.i("main result",mainResult.toString());
+                    Log.i("main result", mainResult.toString());
                     break;
                 case "/":
                     mainResult = division();
-                    Log.i("main result",mainResult.toString());
+                    Log.i("main result", mainResult.toString());
                     break;
                 default:
-                    Log.i("ERROR","NO SUCH OPERATION");
+                    Log.i("ERROR", "NO SUCH OPERATION");
             }
         }
 
     }
 
-    public void clearOperationMemory(){
-        operationMemory.delete(0,operationMemory.length());
+    public void clearOperationMemory() {
+        operationMemory.delete(0, operationMemory.length());
         updateMemoryTextView();
     }
 
-    public void clearResultMemory(){
-        resultMemory.delete(0,resultMemory.length());
+    public void clearResultMemory() {
+        resultMemory.delete(0, resultMemory.length());
         updateResultTextView();
     }
 
-    public void clearRegisters(View view){
+    public void clearRegisters(View view) {
         rej1 = "";
         rej2 = "";
         mainResult = 0.0;
         operator = "";
     }
 
-    public void equalsClicked(View view){
+    public void equalsClicked(View view) {
         clearOperationMemory();
         setRegisters();
         computeResult();
         clearResultMemory();
-        if(rej1.length() == 0 && rej2.length() == 0){
+        if (rej1.length() == 0 && rej2.length() == 0) {
             resultTextView.setText("");
         } else {
-             updateResultTextViewWithResult();
+            updateResultTextViewWithResult();
         }
         clearRegisters(view);
 
     }
 
-    public void bskpClicked(View view){
+    public void bskpClicked(View view) {
         clearOperationMemory();
         setRegisters();
         computeResult();
@@ -201,18 +193,30 @@ public class SimpleCalculator extends AppCompatActivity {
         resultTextView.setText("");
     }
 
-    public void plusMinusClicked(View view){
+    public void plusMinusClicked(View view) {
 
-        if(resultMemory.length()!= 0){
-            if(resultMemory.charAt(0) == '-'){
-                resultMemory.delete(0,1);
+        if (resultMemory.length() != 0) {
+            if (resultMemory.charAt(0) == '-') {
+                resultMemory.delete(0, 1);
             } else {
                 String tempResultMemory = resultMemory.toString();
-                Toast.makeText(SimpleCalculator.this,tempResultMemory.toString(),Toast.LENGTH_LONG).show();
-                resultMemory.delete(0,resultMemory.length());
+                Toast.makeText(SimpleCalculator.this, tempResultMemory.toString(), Toast.LENGTH_LONG).show();
+                resultMemory.delete(0, resultMemory.length());
                 resultMemory.append("-").append(tempResultMemory);
-                Log.i("result memory after",resultMemory.toString());
+                Log.i("result memory after", resultMemory.toString());
             }
+        }
+
+        updateResultTextView();
+
+    }
+
+    public void dotClicked(View view) {
+
+        if (resultMemory.length() == 0) {
+            resultMemory.append("0").append(".");
+        } else if (!(resultMemory.charAt(resultMemory.length() - 1) == '.')) {
+            resultMemory.append(".");
         }
 
         updateResultTextView();
@@ -225,20 +229,30 @@ public class SimpleCalculator extends AppCompatActivity {
         String pressedButtonTag = "";
         pressedButtonTag = pressedButton.getTag().toString();
 
-        if(isNumber(pressedButtonTag)){
+        if (isNumber(pressedButtonTag)) {
 
-            resultMemory.append(pressedButtonTag);
-            updateResultTextView();
+            if (resultMemory.length() > 0) {
+                if (!(pressedButtonTag.equals("0") && resultMemory.charAt(resultMemory.length() - 1) == '0' && resultMemory.length() == 1)) {
+                    resultMemory.append(pressedButtonTag);
+                    updateResultTextView();
+                }
+            } else {
+
+                resultMemory.append(pressedButtonTag);
+                updateResultTextView();
+
+            }
+
 
         }
 
-        if(operationMemory.length() == 0 && resultMemory.length() == 0 && isSign(pressedButtonTag)){
+        if (operationMemory.length() == 0 && resultMemory.length() == 0 && isSign(pressedButtonTag)) {
 
             resultMemory.append("0").append(" ");
 
         }
 
-        if(isSign(pressedButtonTag)){
+        if (isSign(pressedButtonTag)) {
 
             setRegisters();
 
@@ -246,23 +260,22 @@ public class SimpleCalculator extends AppCompatActivity {
 
             setOperator(pressedButtonTag);
 
-            if(rej1.length()!=0 && rej2.length()!=0){
+            if (rej1.length() != 0 && rej2.length() != 0) {
                 updateResultTextViewWithResult();
             }
 
 
-
-            if(resultMemory.length()!=0){
+            if (resultMemory.length() != 0) {
                 operationMemory.append(resultMemory).append(" ");
-                resultMemory.delete(0,resultMemory.length());
+                resultMemory.delete(0, resultMemory.length());
             }
 
-            if(operationMemory.length() != 0 && !isSign(String.valueOf(operationMemory.charAt(operationMemory.length()-2)))){
+            if (operationMemory.length() != 0 && !isSign(String.valueOf(operationMemory.charAt(operationMemory.length() - 2)))) {
                 operationMemory.append(pressedButtonTag).append(" ");
             }
 
-            if(operationMemory.length()!=0 && isSign(String.valueOf(operationMemory.charAt(operationMemory.length()-2)))){
-                operationMemory.delete(operationMemory.length()-2,operationMemory.length());
+            if (operationMemory.length() != 0 && isSign(String.valueOf(operationMemory.charAt(operationMemory.length() - 2)))) {
+                operationMemory.delete(operationMemory.length() - 2, operationMemory.length());
                 operationMemory.append(pressedButtonTag).append(" ");
             }
 
@@ -284,14 +297,14 @@ public class SimpleCalculator extends AppCompatActivity {
 
         String mainResultSting = "";
 
-        try{
+        try {
             Integer mainResultInteger = Integer.parseInt(mainResult.toString());
             mainResultSting = mainResultInteger.toString();
-        } catch (Exception e){
-            Log.i("ERROR","I CAN'T PARSE TO INT");
+        } catch (Exception e) {
+            Log.i("ERROR", "I CAN'T PARSE TO INT");
             mainResultSting = mainResult.toString();
-            if(mainResultSting.length() > 11){
-                mainResultSting = mainResultSting.substring(0,11);
+            if (mainResultSting.length() > 11) {
+                mainResultSting = mainResultSting.substring(0, 11);
             }
         } finally {
             if (mainResultSting.length() < 11) {
@@ -313,7 +326,7 @@ public class SimpleCalculator extends AppCompatActivity {
 
     int clickCounter = 0;
 
-    public void doubleClick(View view){
+    public void doubleClick(View view) {
 
         clickCounter++;
 
@@ -321,18 +334,18 @@ public class SimpleCalculator extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                if (clickCounter == 1){
-                    if(resultMemory.length()>0){
-                        resultMemory = resultMemory.delete(resultMemory.length()-1,resultMemory.length());
+                if (clickCounter == 1) {
+                    if (resultMemory.length() > 0) {
+                        resultMemory = resultMemory.delete(resultMemory.length() - 1, resultMemory.length());
                         updateResultTextView();
                     }
-                } else if (clickCounter == 2){
-                    resultMemory = resultMemory.delete(0,resultMemory.length());
+                } else if (clickCounter == 2) {
+                    resultMemory = resultMemory.delete(0, resultMemory.length());
                     updateResultTextView();
                 }
                 clickCounter = 0;
             }
-        },500);
+        }, 500);
 
 
     }
